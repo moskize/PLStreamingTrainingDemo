dd5b7b18fdc0fc9c2d10e06bceb0c6c8a1e1b028
moskize <xiangtantaozeyu@icloud.com>
2016-05-19 16:15:22 +0800

    init.

---
+++ PLStreamingTrainingDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PLStreamingTrainingDemo
+//
+//  Created by TaoZeyu on 16/5/19.
+//  Copyright © 2016年 taozeyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ PLStreamingTrainingDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PLStreamingTrainingDemo
+//
+//  Created by TaoZeyu on 16/5/19.
+//  Copyright © 2016年 taozeyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
